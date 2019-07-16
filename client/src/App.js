51956d119f455b554c82053947f403d18d437e95
Andy Weiss <andyweiss1982@gmail.com>
2019-07-16 15:34:19 -0400

    glue it together

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './App.css'
 import Hunger from './Hunger/Hunger'
+import Results from './components/Results/Results'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 class App extends React.Component {
   render(){
     return(
-      <Hunger/>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Hunger}/>
+          <Route exact path="/results" component={Results} />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
--- client/src/Hunger/Hunger.js
+++ client/src/Hunger/Hunger.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import './Hunger.css'
+import { Link } from 'react-router-dom'
 
 class Hunger extends React.Component {
-  state = {
-    background: 'panda',
-    latitude: null,
-    longitude: null,
-    restaurants: null
-  }
+  state = { background: 'panda', }
 
   componentDidMount(){
     this.interval = setInterval(this.changeBackground, 3000)
-    this.getPosition()
-  }
-
-  getPosition = () => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ latitude: position.coords.latitude, longitude: position.coords.longitude}),
-      () => console.log("Something went wrong"),
-      {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-      }
-    )
-  }
-
-  fetchRestaurants = () => {
-    console.log(this.state)
-    fetch(`/api/${this.state.latitude}/${this.state.longitude}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        this.setState({ restaurants: data.businesses })
-      });
   }
 
   changeBackground = () => {
@@ -44,14 +17,14 @@ class Hunger extends React.Component {
   }
 
   render(){
-    
+
     return (
       <React.Fragment>
-        <div
-        className={this.state.background}
-        onClick={this.fetchRestaurants}>
+        <div className={this.state.background}>
           <div id='shrink'>
-            <button id='title' unselectable="on" className='unselectable' >Hangry!</button>
+            <Link to="/results">
+              <button id='title' unselectable="on" className='unselectable' >Hangry!</button>
+            </Link>
           </div>
         </div>
       </React.Fragment>
